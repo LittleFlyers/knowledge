23c1d76813dd936a0d61b3f6f087cfba5850119c
LittleFlyers <2538285284@qq.com>
2017-07-24 14:59:16 +0800

    update experience.php

---
+++ application/knowledge/controller/Experience.php
@@ -0,0 +1,30 @@
+<?php
+namespace app\knowledge\controller;
+
+class Dictionary{
+	//获取经验的全部信息
+	public function all()
+	{
+		
+	}
+	//获取单个经验
+	public function one()
+	{
+		
+	}
+	//回答经验
+	public function answer()
+	{
+		
+	}
+	//添加经验
+	public function add()
+	{
+		
+	}
+    //删除经验
+	public function delete()
+	{
+		
+	}
+}
