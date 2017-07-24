9b81753ec5c315e90b4d1dff4a4a55cfec98ce4c
LittleFlyers <2538285284@qq.com>
2017-07-25 01:44:50 +0800

    123

--- application/knowledge/controller/Consult.php
+++ application/knowledge/controller/Consult.php
@@ -5,7 +5,26 @@ class Consult{
 	//获取咨询中的全部信息
 	public function all()
 	{
-		
+		$access_token = input('post.access_token');
+        $user_id = get_user_id_by_access_token($access_token);
+
+        $Consult = db('consult');
+        $emap['user_id'] = $user_id;
+        $consult_list = $Consult->where($emap)->find();
+		if($consult_list)
+        {
+            $result['err_code'] = 0;
+            $result['err_msg'] = 'ok';
+            $result['data'] = $consult_list;
+        }
+        else
+        {
+            $result['err_code'] = 1;
+            $result['err_msg'] = '暂无';
+        }
+
+        return json_encode($result);
+
 	}
 	//获取咨询中详细信息
 	public function one()
