108c99010f23f4a0ba1e0482029ae6341a4b7da0
LittleFlyers <2538285284@qq.com>
2017-07-24 20:08:35 +0800

    1

--- application/knowledge/controller/Experience.php
+++ application/knowledge/controller/Experience.php
@@ -34,7 +34,23 @@ class Experience{
 	//添加经验
 	public function add()
 	{
-		
+		$access_token = input('post.access_token');
+        $exp_user_id = get_user_id_by_access_token($access_token);
+
+		$title = input('post.title');
+        $answer = input('post.answer');
+        $type = input('post.type');
+
+        $Experience = db('experience');
+        $emap['experience_title'] = $title;
+		$emap['user_id'] = $exp_user_id;
+		$emap['type'] = $type;
+        $emap['create_date'] = date('Y-m-d G:i:s');
+        $Experience->insert($emap);
+
+        $result['err_code'] = 0;
+        $result['err_msg'] = 'ok';
+        return json_encode($result);
 	}
     //删除经验
 	public function delete()
