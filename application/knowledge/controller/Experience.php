9f8d79f515bd1475fd41b4a33292f286f9784306
LittleFlyers <2538285284@qq.com>
2017-07-24 19:35:07 +0800

    1

--- application/knowledge/controller/Experience.php
+++ application/knowledge/controller/Experience.php
@@ -5,7 +5,21 @@ class Experience{
 	//获取经验的全部信息
 	public function all()
 	{
-		
+		$Meeting = db('experience');
+        $meeting_list = $Meeting->select();
+        if($meeting_list)
+        {
+            $result['err_code'] = 0;
+            $result['err_msg'] = 'ok';
+            $result['data'] = $meeting_list;
+        }
+        else
+        {
+            $result['err_code'] = 1;
+            $result['err_msg'] = '暂无';
+        }
+
+        return json_encode($result);
 	}
 	//获取单个经验
 	public function one()
