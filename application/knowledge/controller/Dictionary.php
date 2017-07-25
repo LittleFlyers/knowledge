09f935aac13e55172a5aa6aeb18d24add56ad89d
LittleFlyers <2538285284@qq.com>
2017-07-25 16:18:59 +0800

    1

--- application/knowledge/controller/Dictionary.php
+++ application/knowledge/controller/Dictionary.php
@@ -1,11 +1,11 @@
 <?php
 namespace app\knowledge\controller;
-
+ 
 class Dictionary{
-	//获取词典中全部的信息
-	public function all()
-	{
-	   $Meeting = db('dictionary');
+    //获取词典中全部的信息
+    public function all()
+    {
+       $Meeting = db('dictionary');
         $meeting_list = $Meeting->select();
         if($meeting_list)
         {
@@ -18,53 +18,53 @@ class Dictionary{
             $result['err_code'] = 1;
             $result['err_msg'] = '暂无';
         }
-
+ 
         return json_encode($result);
-	}
-	//获取词典中的单个词条
-	public function one()
-	{
-		
-	}
-	//向词典中添加词条
-	public function add()
-	{
-		$word = input('post.word');
-		$type = input('post.type');
-		$explain = input('post.explain');
-
-		$Dw = db('dictionary');
-		$emap['word'] = $word;
-		$emap['word_explain'] = $explain;
-		$emap['type'] = $type;
-		$Dw->insert($emap);
-		$result['err_code'] = 0;
+    }
+    //获取词典中的单个词条
+    public function one()
+    {
+         
+    }
+    //向词典中添加词条
+    public function add()
+    {
+        $word = input('post.word');
+        $type = input('post.type');
+        $explain = input('post.explain');
+ 
+        $Dw = db('dictionary');
+        $emap['word'] = $word;
+        $emap['word_explain'] = $explain;
+        $emap['type'] = $type;
+        $Dw->insert($emap);
+        $result['err_code'] = 0;
         $result['err_msg'] = 'ok';
         return json_encode($result);
-		
-	}
-	//接收词条图片
-	public function load()
-	{
+         
+    }
+    //接收词条图片
+    public function load()
+    {
         $file = request()->file('img');
-    
+     
         // 移动到框架应用根目录/public/uploads/ 目录下
-        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+        $info = $file->move('C:\xampp\htdocs\InterviewAdd\public\uploads');
         if($info){
             $result['err_msg'] = 'ok';
         }else{
             $result['err_msg'] = 'failt';
         }
-		return json_encode($result);
-	}
-	//更新词条
-	public function update()
-	{
-		
-	}
+        return json_encode($result);
+    }
+    //更新词条
+    public function update()
+    {
+         
+    }
     //删除词条
-	public function delete()
-	{
-		
-	}
+    public function delete()
+    {
+         
+    }
 }
